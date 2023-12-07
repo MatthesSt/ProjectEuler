252ea4acf0ebd5f007546a9daf7215e6fcd9793b
MatthesSt <m.staacken@mbd-team.de>
2023-12-07 15:16:32 +0100

    16 finished

--- problem16.mjs
+++ problem16.mjs
@@ -1,22 +1,10 @@
 // 2 ** 32 =  32768 => 2+3+7+6+8 = 26
 // 2 ** 1000 ??
 
-let currentValue = "2";
-for (let i = 1; i < 11; i++) {
-  currentValue = mul(currentValue, currentValue);
-}
+import { addNumStrings, mulNumStrings, mulNumStringArr } from "./utils.mjs";
 
-//FIXME:
-function mul(a, b) {
-  let currentPro = "";
-  let bigger = a.length > b.length ? a : b;
-  let hangover = 0;
-  for (let charIndex in bigger) {
-    let digitPro = Number(a[a.length - charIndex - 1] || 1) * Number(b[b.length - charIndex - 1] || 1);
-    currentPro = (digitPro % 10) + hangover + currentPro;
-    hangover = Math.floor(digitPro / 10);
-  }
-  return hangover ? hangover + currentPro : currentPro;
-}
-
-console.log(mul("1024", "356"));
+console.log(
+  mulNumStringArr(Array(1000).fill("2"))
+    .split("")
+    .reduce((a, b) => +a + +b, 0)
+);
--- utils.mjs
+++ utils.mjs
@@ -8,3 +8,38 @@ export function chunk(arr, len) {
 
   return chunks;
 }
+
+export function addNumStrings(arr) {
+  arr = arr.filter((e) => e.length != 0);
+  let currentSum = "";
+  if (arr.length < 2) return arr[0] || 0;
+  let bigger = arr.sort((a, b) => b.length - a.length)[0];
+  let hangover = [];
+  for (let charIndex in bigger) {
+    let digitSum = arr.reduce((a, c) => a + Number(c[c.length - charIndex - 1] || 0), 0);
+    let digit = Number(digitSum) + Number(hangover.pop() || "");
+    currentSum = (digit % 10) + currentSum;
+    hangover = addNumStrings([hangover.join(""), Math.floor(digit / 10).toString()]).split("");
+  }
+  return (hangover.length != 0 ? hangover.join("") + currentSum : currentSum).replace(/^0+/, "");
+}
+
+export function mulNumStrings(a, b) {
+  let currentPro = "";
+  let [bigger, smaller] = a.length >= b.length ? [a, b] : [b, a];
+  for (let charIndex in smaller) {
+    for (let i = 0; i < Number(smaller[charIndex]); i++) {
+      currentPro = addNumStrings([currentPro, ...Array(10 ** (smaller.length - 1 - charIndex)).fill(bigger)]);
+    }
+  }
+  return currentPro;
+}
+export function mulNumStringArr(arr) {
+  arr = arr.filter((e) => e.length != 0 && e != 1);
+  if (arr.length < 2) return arr[0] || 0;
+  let currentPro = arr.pop();
+  for (let num of arr) {
+    currentPro = mulNumStrings(currentPro, num);
+  }
+  return currentPro;
+}
